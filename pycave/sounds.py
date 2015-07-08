69ec41e7f52d0313a49257cb5b8bf83f8ea7b47e
William Hicks <william_hicks@alumni.brown.edu>
2015-07-08 18:10:28 -0400

    Provide CaveSound XML I/O
    Resolve issue 53
    Resolve issue 54

--- pycave/sounds.py
+++ pycave/sounds.py
@@ -1,7 +1,10 @@
 """Tools for working with sounds in Cave projects
 """
+import xml.etree.ElementTree as ET
 from features import CaveFeature
 from validators import AlwaysValid, IsNumeric, OptionListValidator
+from errors import ConsistencyError, BadCaveXML
+from xml_tools import bool2text, text2bool
 
 
 class CaveSound(CaveFeature):
@@ -45,7 +48,42 @@ class CaveSound(CaveFeature):
 
         :param :py:class:xml.etree.ElementTree.Element all_sounds_root
         """
-        CaveFeature.toXML(self, all_sounds_root)  # TODO: Replace this
+        attrib = {}
+        try:
+            attrib["name"] = str(self["name"])
+        except KeyError:
+            raise ConsistencyError(
+                'CaveSound must set a value for "name" key')
+        try:
+            attrib["filename"] = str(self["filename"])
+        except KeyError:
+            raise ConsistencyError(
+                'CaveSound must set a value for "filename" key')
+        if not self.is_default("autostart"):
+            attrib["autostart"] = bool2text(self["autostart"])
+        sound_root = ET.SubElement(all_sounds_root, "Sound", attrib=attrib)
+
+        node = ET.SubElement(sound_root, "Mode")
+        ET.SubElement(node, self["movement_mode"])
+
+        node = ET.SubElement(sound_root, "Repeat")
+        if self["repetitions"] == 0:
+            ET.SubElement(node, "NoRepeat")
+        if self["repetitions"] < 0:
+            ET.SubElement(node, "RepeatForever")
+        if self["repetitions"] > 0:
+            node = ET.SubElement(node, "RepeatNum")
+            node.text = str(self["repetitions"])
+
+        settings = {}
+        attrib_map = {
+            "frequency_scale": "freq", "volume_scale": "volume", "pan": "pan"}
+        for key, xml_attrib in attrib_map:
+            if not self.is_default(key):
+                settings[xml_attrib] = str(self[key])
+        node = ET.SubElement(sound_root, "Settings", attrib=settings)
+
+        return sound_root
 
     @classmethod
     def fromXML(sound_root):
@@ -53,7 +91,62 @@ class CaveSound(CaveFeature):
 
         :param :py:class:xml.etree.ElementTree.Element sound_root
         """
-        return CaveFeature.fromXML(sound_root)  # TODO: Replace this
+        new_sound = CaveSound()
+        try:
+            new_sound["name"] = sound_root.attrib["name"]
+        except KeyError:
+            raise BadCaveXML(
+                "Sound node must specify name attribute")
+        try:
+            new_sound["filename"] = sound_root.attrib["filename"]
+        except KeyError:
+            raise BadCaveXML(
+                "Sound node must specify filename attribute")
+        if "autostart" in sound_root.attrib:
+            new_sound["autostart"] = text2bool(sound_root.attrib["autostart"])
+
+        movement_node = sound_root.find("Mode")
+        if movement_node is None:
+            raise BadCaveXML(
+                "Sound node must contain Mode child node")
+        for mode in new_sound.argument_validators[
+                "movement_mode"].valid_options:
+            if movement_node.find(mode) is not None:
+                new_sound["movement_mode"] = mode
+                break
+        if "movement_mode" not in new_sound:
+            raise BadCaveXML(
+                "Mode node must contain child node specifying a valid mode"
+                )
+
+        repeat_node = sound_root.find("Repeat")
+        if repeat_node is None:
+            raise BadCaveXML(
+                "Sound node must contain Repeat child node")
+        if repeat_node.find("NoRepeat") is not None:
+            new_sound["repetitions"] = 0
+        elif repeat_node.find("RepeatForever") is not None:
+            new_sound["repetitions"] = -1
+        else:
+            repeat_node = repeat_node.find("RepeatNum")
+            try:
+                new_sound["repetitions"] = int(repeat_node.text.strip())
+            except AttributeError:
+                raise BadCaveXML(
+                    "Repeat node must contain child node specifying"
+                    " repetitions")
+
+        settings_node = sound_root.find("Settings")
+        if settings_node is None:
+            raise BadCaveXML(
+                "Sound node must have Settings child node")
+        xml_map = {
+            "freq": "frequency_scale", "volume": "volume_scale", "pan": "pan"}
+        for key, value in xml_map:
+            if key in settings_node.attrib:
+                new_sound[value] = float(settings_node.attrib[key])
+
+        return new_sound
 
     def blend(self):
         """Create representation of CaveSound in Blender"""
