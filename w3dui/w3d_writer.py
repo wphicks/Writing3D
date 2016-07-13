f165e81a8b1397205eeeea9bde57e804c759c3a0
William Hicks <william.p.hicks@gmail.com>
2016-07-13 19:35:31 -0400

    Add tabbed notebook for toplevel editor

--- w3dui/w3d_writer.py
+++ w3dui/w3d_writer.py
@@ -1,11 +1,28 @@
 import flask
+import webbrowser
+import threading
 
 writer = flask.Flask("W3D Writer")
 
 
+class EditorTab(object):
+    """Top-level tab for W3D Writer"""
+    def __init__(self, name):
+        self.name = name
+
+
 @writer.route("/")
 def main():
-    return flask.render_template('index.html')
+    return flask.render_template(
+        'index.html',
+        toptabs=[
+            EditorTab("Global"),
+            EditorTab("Objects"),
+            EditorTab("Timelines")
+        ]
+    )
 
 if __name__ == "__main__":
-    writer.run()
+    server_thread = threading.Thread(target=writer.run)
+    server_thread.start()
+    webbrowser.open_new_tab("http://localhost:5000")
