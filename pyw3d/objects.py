wphicks/Writing3D

commit 7d9c8ca9530a5fb5648103915b1a5540d3731f41

Load fonts only once
Prevent the same font from being loaded multiple times into Blend file

pyw3d/objects.py
@@ -366,6 +366,8 @@ class W3DText(W3DContent):
 
     ui_order = ["text", "halign", "valign", "font", "depth"]
 
+    _loaded_fonts = set()
+
     def toXML(self, object_root):
         """Store W3DText as Content node within Object node
 
@@ -420,14 +422,16 @@ class W3DText(W3DContent):
         bpy.ops.object.text_add(rotation=(math.pi / 2, 0, 0))
         new_text_object = bpy.context.object
         new_text_object.data.body = self["text"]
-        # TODO: Get proper font directory
-        if self["font"] is not None:
+        if (
+                self["font"] is not None and self["font"] not in
+                self._loaded_fonts):
             try:
                 new_text_object.data.font = bpy.data.fonts.load(self["font"])
             except:
                 new_text_object.data.font = bpy.data.fonts.load(
                     os.path.join("fonts", self["font"])
                 )
+            self._loaded_fonts.add(self["font"])
         new_text_object.data.extrude = self["depth"]
         new_text_object.data.fill_mode = "BOTH"
         new_text_object.data.align = self["halign"].upper()

pyw3d/features.py
@@ -96,7 +96,7 @@ class W3DFeature(dict):
             try:
                 if self[key] != other[key]:
                     return False
-            except KeyError:
+            except (KeyError, ConsistencyError):
                 return False
         return True
 

commit b981bdaaf5db4b1380242aec2aadbaadeb25c29a

Make objects with same font use correct font
Bugfix for issue introduced in 7d9c8ca9530a5f

pyw3d/objects.py
@@ -366,7 +366,7 @@ class W3DText(W3DContent):
 
     ui_order = ["text", "halign", "valign", "font", "depth"]
 
-    _loaded_fonts = set()
+    _loaded_fonts = {}
 
     def toXML(self, object_root):
         """Store W3DText as Content node within Object node
@@ -431,7 +431,9 @@ class W3DText(W3DContent):
                 new_text_object.data.font = bpy.data.fonts.load(
                     os.path.join("fonts", self["font"])
                 )
-            self._loaded_fonts.add(self["font"])
+            self._loaded_fonts[self["font"]] = new_text_object.data.font
+        else:
+            new_text_object.data.font = self._loaded_fonts[self["font"]]
         new_text_object.data.extrude = self["depth"]
         new_text_object.data.fill_mode = "BOTH"
         new_text_object.data.align = self["halign"].upper()