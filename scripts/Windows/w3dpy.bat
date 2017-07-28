4b44165afe3188efe95d0bcf069f4404b53677c4
William Hicks <william.p.hicks@gmail.com>
2017-07-28 17:12:57 -0400

    Add Blender site modification for Windows

--- scripts/Windows/w3dpy.bat
+++ scripts/Windows/w3dpy.bat
@@ -4,4 +4,5 @@ cd /d %~dp0
 for /f %%i in ('findblender.bat') do set blenderexec=%%i
 if "%blenderexec%" == "Not found" set blenderexec="%cd%..\..\..\blender\blender.exe"
 echo "INVOCATION >> %blenderexec% --background --python Writing3D\samples\cwapp.py -- %*"
+%blenderexec%  --background --python "%cd%..\amend_blender_site.py"
 %blenderexec%  --background --python %*
