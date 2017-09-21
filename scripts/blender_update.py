df1f387b0d048c040272e9fbde50e35ad0a0da35
William Hicks <william.p.hicks@gmail.com>
2017-09-21 17:55:26 -0400

    Correct install path for Blender

--- scripts/blender_update.py
+++ scripts/blender_update.py
@@ -4,6 +4,7 @@ import platform
 import urllib.request
 import zipfile
 import tarfile
+import shutil
 import bpy
 
 REQUIRED_VERSION = (2, 79, 0)
@@ -21,7 +22,7 @@ INSTALL_PATH = os.path.abspath(
     os.path.normpath(
         os.path.join(
             os.path.dirname(__file__),
-            os.pardir
+            os.pardir, os.pardir
         )
     )
 )
@@ -36,15 +37,11 @@ def get_platform():
         return "Linux"
 
 
-def update_blender():
-    if REQUIRED_VERSION < bpy.app.version:
-        return
-    print("Updating Blender...")
+def download_blender():
     cur_platform = get_platform()
     install_filename, headers = urllib.request.urlretrieve(
         DOWNLOAD_URLS[cur_platform]
     )
-    print("Download complete")
     if cur_platform == "Windows":
         zipfile.ZipFile(install_filename).extractall(path=INSTALL_PATH)
     elif cur_platform == "Mac":
@@ -53,6 +50,26 @@ def update_blender():
     else:
         with tarfile.open(install_filename, "r:bz2") as archive:
             archive.extractall(path=INSTALL_PATH)
+
+
+def update_blender():
+    print(bpy.app.version)
+    if REQUIRED_VERSION < bpy.app.version:
+        return
+    print("Updating Blender...")
+    download_blender()
+    blend_dir = os.path.join(INSTALL_PATH, "blender")
+    old_config = os.path.join(os.path.expanduser("~"), ".w3d.json")
+    new_blend_dir = [
+        file_ for file_ in os.listdir(INSTALL_PATH) if
+        os.path.isdir(os.path.join(INSTALL_PATH, file_)) and
+        file_.startswith("blender-{}.{}".format(*REQUIRED_VERSION[:-1]))
+    ][0]
+    if os.path.isdir(blend_dir):
+        shutil.rmtree(blend_dir)
+    if os.path.isfile(old_config):
+        shutil.move(old_config, "{}.old".format(old_config))
+    shutil.move(new_blend_dir, blend_dir)
 
 
 if __name__ == "__main__":
