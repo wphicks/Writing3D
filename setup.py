c5cf4f3b5f36bc3e8abafdbc513be4b7ede26713
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 13:28:09 +0000

    Adding tarfile member sanitization to extractall()

--- setup.py
+++ setup.py
@@ -247,7 +247,26 @@ class BlenderInstaller(object):
             with tarfile.open(self.archive_name, "r:bz2") as install_file:
                 self.blender_directory = os.path.join(
                     self.install_directory, install_file.next().name)
-                install_file.extractall(path=self.install_directory)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(install_file, path=self.install_directory)
         else:
             with zipfile.ZipFile(self.archive_name) as install_file:
                 self.blender_directory = os.path.splitext(self.archive_name)[0]
